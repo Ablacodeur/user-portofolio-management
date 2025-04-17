b2d84f796e69ce0199e1a5b0c347a64412595683
Ablacodeur <agbolivicky@gmail.com>
2025-04-17 18:39:59 -0400

    add sign in page frontend

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignIn from './SignIn/SignIn'
+import './App.css'
+import { Box } from '@mui/joy'
+export default function App() {
+  return (
+    <Box sx={{ height: '90vh',}}>
+      <SignIn />
+    </Box>
+  )
+}
