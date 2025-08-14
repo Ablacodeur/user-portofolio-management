5c21743762903356a1440e9aa3801e51af3845c7
Ablacodeur <agbolivicky@gmail.com>
2025-08-14 15:54:36 -0400

    browser design and code v1

--- src/Browser/Browser.jsx
+++ src/Browser/Browser.jsx
@@ -13,10 +13,11 @@ import { SearchBar } from "../SearchBar/SearchBar";
 import SlideSection from "../SlideSection/SlideSection";
 import Footer from "../Footer/Footer";
 import { FilterBar } from "../FilterBar/FilterBar";
+import { ScrollTrigger } from "gsap/all";
 export default function Browser() {
   const imageContainerRef = useRef(null);
   const titleRef = useRef(null); 
-
+  const typingTextRef = useRef(null);
   useEffect(() => {
     // Animation GSAP pour fade-in et fade-out du texte
     gsap.fromTo(
@@ -79,6 +80,27 @@ export default function Browser() {
       },
     });
   }, []);
+  useEffect(() => {
+    const typingElement = typingTextRef.current;
+
+    // Animation GSAP pour l'effet zoom-in
+    gsap.fromTo(
+      typingElement,
+      { scale: 0.8, opacity: 0 }, // Départ : réduit et invisible
+      {
+        scale: 1, // Taille normale
+        opacity: 1, // Complètement visible
+        duration: 1, // Durée de l'animation
+        ease: "power2.out", // Transition fluide
+        scrollTrigger: {
+          trigger: typingElement, // Déclencheur : le texte
+          start: "top 80%", // L'animation commence lorsque le texte entre dans la vue
+          end: "top 50%", // L'animation se termine lorsque le texte atteint 50% de la hauteur de la fenêtre
+          scrub: true, // Synchronise l'animation avec le défilement
+        },
+      }
+    );
+  }, []);  
   return (
     <Box
       sx={{
@@ -148,10 +170,14 @@ export default function Browser() {
       </Box>
       </Box>
       <Box sx={{ width: "100%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
-        <Typography variant="h5" sx={{ textAlign: "center", marginTop: "20px", width: "80%", color: "#041458" }}>
-           <FormatQuoteOutlinedIcon /> A portfolio is a museum of your work, with past tech stacks, case studies, and your work history therefore we make sure to showcase your work
-        </Typography>
-      </Box>
+      <Typography
+        ref={typingTextRef}
+        variant="h5"
+        sx={{ textAlign: "center", marginTop: "20px", width: "80%", color: "#041458" }}
+      >
+        <FormatQuoteOutlinedIcon /> {/* Icône avant le texte */}
+        A portofolio is a collection of your best work, showcasing your skills and creativity. It is a powerful tool to demonstrate your expertise and attract potential clients or employers.
+      </Typography>      </Box>
       <Box sx={{width: '100%', display: 'flex', justifyContent: 'center', marginTop: '250px',alignItems: 'center',textAlign: 'center',marginBottom:
       '150px'}}>
         <FilterBar/>
